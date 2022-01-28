66e357cb3b89989e64561f42e6e9f8ea23a67821
Supasin Saetang <supasin.s1t@gmail.com>
2022-01-28 13:24:55 +0700

    update modal

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ let btnModal = document.querySelectorAll(".btn-modal");
 let ModalShow = document.querySelector(".float-modal-container");
 let btnClose = document.querySelector(".btn-close");
 let btnDone = document.querySelector(".btn-receive");
+let date = document.querySelector("#date");
+let time = document.querySelector("#time");
 
 for (let i = 0; i < btnModal.length; i++) {
   // click open to modal
   btnModal[i].addEventListener("click", () => {
     console.log(`button no : ${i}`);
     ModalShow.style.display = "flex";
-
+    date.innerHTML = "Date : " + new Date().toLocaleDateString();
+    time.innerHTML = "Time : " + new Date().toLocaleTimeString();
     // countdown --> close modal auto (15sec)
-    let timeOut = 10;
+    let timeOut = 5;
     let countDownAuto = setInterval(countDown, 1000);
     function countDown() {
       let counting = timeOut--;
